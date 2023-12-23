49399e5852b46553f28b92a0e992bfd2dc1697ff
BrunoJMALima <bj_mal@hotmail.com>
2023-12-23 10:19:36 -0300

    inicio do sistema

---
+++ src/main/java/com/mycompany/sistemahoel/SistemaHoel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemahoel;
+
+/**
+ *
+ * @author User
+ */
+public class SistemaHoel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
